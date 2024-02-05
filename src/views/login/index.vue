ed20cabe8e1c2eba40057da362b5dd5ccc3361fe
shizhanwang <shizhanwang30@gmail.com>
2024-02-05 20:24:46 +0800

    更新依赖包和登录页面***
    ***更新依赖包和登录页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -37,11 +37,6 @@
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;"
         @click.native.prevent="handleLogin">登录</el-button>
 
-      <div class="tips">
-        <span style="margin-right:20px;">用户名：admin</span>
-        <span> 密码：任何密码都行</span>
-      </div>
-
     </el-form>
   </div>
 </template>
