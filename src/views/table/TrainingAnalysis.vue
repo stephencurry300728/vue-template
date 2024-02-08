8465374da37657948a25a5b8f684f05895bfc503
shizhanwang <shizhanwang30@gmail.com>
2024-02-08 22:26:09 +0800

    更新培训数据的展示和统计

--- src/views/table/TrainingAnalysis.vue
+++ src/views/table/TrainingAnalysis.vue
@@ -4,8 +4,8 @@
 
             <div class="text-center">
                 <span class="training-line"> {{ processedData.lineNames }}</span>
-                共计参加<span class="training-items">{{ processedData.trainingItems }}</span>
-                培训人数为:<span class="total-count">{{ processedData.totalCount }}</span>人
+                共计参加 <span class="training-items">{{ processedData.trainingItems }}</span>
+                培训人数为：<span class="total-count">{{ processedData.totalCount }}</span>人
                 合格率：<span class="pass-rate">{{ processedData.passRate }}%</span>
             </div>
 
@@ -37,7 +37,7 @@ export default {
         ...mapState('table', [
             'trainingAnalysisData',
         ]),
-        // 处理培训数据
+        // 培训数据的总计合格率概况
         processedData() {
             // 检查 trainingAnalysisData 是否存在或是否为空数组
             if (!this.trainingAnalysisData || this.trainingAnalysisData.length === 0) {
@@ -50,13 +50,14 @@ export default {
             // new Set() 是创建一个新的 Set 对象。Set 是一种特殊的数据结构，它只存储唯一的值，重复的值会被忽略
             // sort() 方法是对数组进行排序，最后返回的是['09', '10']
             const trainLines = [...new Set(data.map(item => item.trainLines))].sort();
+            // 获取培训总人数
             const totalCount = data.length;
-            // 通过筛选出 assessment_result 为 2(合格) 或 3(优秀) 的项，然后计算合格率
+            // 通过筛选出 assessment_result 为 2(合格) 或 3(优秀) 的项，统计为合格人数
             const passCount = data.filter(item => item.assessment_result === 2 || item.assessment_result === 3).length;
             // 计算合格率
             const passRate = ((passCount / totalCount) * 100).toFixed(2);
 
-            // 将线路编号转换为中文
+            // 将线路的值转换为中文
             const lineNames = trainLines.map(line => {
                 switch (line) {
                     case '01': return '1号线';
@@ -65,22 +66,23 @@ export default {
                     case '10': return '10号线';
                     default: return `${line}号线`;
                 }
-            }).join('/');
+            }).join(' / ');
 
             // 组合 train_model 和 assessment_item，并去除重复的组合
-            // 将所有组合连接成一个字符串，组合之间用 , 分隔展示不同考核项目
-            const trainingItems = [...new Set(data.map(item => `${item.train_model}-${item.assessment_item}`))].join(', ');
+            // 将科目连接成一个字符串并用，分隔展示不同考核项目
+            const trainingItems = [...new Set(data.map(item => `${item.train_model}-${item.assessment_item}`))].join('， ');
 
             return { lineNames, totalCount, passRate, trainingItems };
         },
 
+        // 表格数据展示不同乘务班组的合格信息
         tableData() {
             // 首先检查是否有 trainingAnalysisData 数据，如果没有或为空数组，则直接返回一个空数组
             if (!this.trainingAnalysisData || this.trainingAnalysisData.length === 0) {
                 return [];
             }
 
-            // 使用 reduce 方法遍历 trainingAnalysisData 数组，构建一个以 crew_group 为键，统计信息为值的对象（groupStats）
+            // 使用 reduce 方法遍历 trainingAnalysisData 数组，构建一个以 crew_group（乘务班组） 为键，统计信息为值的对象
             const groupStats = this.trainingAnalysisData.reduce((acc, item) => {
                 // 检查累加器对象（acc）中是否已经有当前项的 crew_group 作为键的对象
                 // 如果没有，则初始化该 crew_group 的统计信息对象，包括人数（count）、优秀（excellent）、合格（qualified）、不合格（unqualified）计数
@@ -105,10 +107,15 @@ export default {
             // Object.keys(groupStats) 生成一个包含所有 crew_group 键的数组
             // map 方法遍历这个键数组，为每个 crew_group 构建一个包含统计信息的新对象，最后的百分比保留两位小数
             return Object.keys(groupStats).map(group => ({
+                // 乘务班组
                 crew_group: group,
+                // 人数总数
                 count: groupStats[group].count,
+                // 优秀的总数（百分比）
                 excellent: `${groupStats[group].excellent} (${((groupStats[group].excellent / groupStats[group].count) * 100).toFixed(2)}%)`,
+                // 合格的总数（百分比）
                 qualified: `${groupStats[group].qualified} (${((groupStats[group].qualified / groupStats[group].count) * 100).toFixed(2)}%)`,
+                // 不合格的总数（百分比）
                 unqualified: `${groupStats[group].unqualified} (${((groupStats[group].unqualified / groupStats[group].count) * 100).toFixed(2)}%)`,
             }));
         },
