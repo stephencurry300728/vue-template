9acc6c73b133b62b9c7dc2f3448e5c00eef80990
shizhanwang <shizhanwang30@gmail.com>
2024-02-27 13:02:03 +0800

    修复保存分类的URL错误

--- src/api/settings.js
+++ src/api/settings.js
@@ -2,7 +2,7 @@ import request from '@/utils/request';
 
 export function SaveClassification(data) {
     return request({
-        url: '/save-classification/', // 根据实际URL调整
+        url: '/save-classification/',
         method: 'post',
         data
     });
